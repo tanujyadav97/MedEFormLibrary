5b4b9287440e50291e106d3340a094529a45fea7
Tanuj <tanujyadav171997@gmail.com>
2019-04-13 18:37:59 +0530

    Initial commit

---
+++ app/src/main/java/com/hackslash/medeformlibrary/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hackslash.medeformlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GenerateForm gf = new GenerateForm(MainActivity.this, this,
+                "Android/data/com.android.hackslash.openehr",
+                "openEHR-EHR-OBSERVATION.body_temperature.v2.adl", "OBSERVATION", R.id.llroot, "patientid");
+        gf.makeForm();
+    }
+}
