b71401684a17d2a3a4a877576aae98f07aeca8ab
Tanuj <tanujyadav171997@gmail.com>
2019-04-17 00:18:13 +0530

    Add more queries

--- app/src/main/java/com/hackslash/medeformlibrary/MainActivity.java
+++ app/src/main/java/com/hackslash/medeformlibrary/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void doQuery() {
         Queries qry = new Queries();
+
         ArrayList<QueryObj> queries1 = new ArrayList<>();
         queries1.add(new QueryObj(queryType.WHERE_GREATER_THAN, "Hb Count", 0));
         queries1.add(new QueryObj(queryType.WHERE_LESS_THAN, "Hb Count", 700));
@@ -44,10 +45,62 @@ public class MainActivity extends AppCompatActivity {
                 });
 
         ArrayList<QueryObj> queries2 = new ArrayList<>();
-        queries2.add(new QueryObj(queryType.WHERE_EQUAL_TO, "Source of Referral", "Mohit yadav"));
+        queries2.add(new QueryObj(queryType.WHERE_EQUAL_TO, "Source of Referral", "Hospital"));
 //        queries2.add(new QueryObj(queryType.WHERE_EQUAL_TO,"Grade","B"));
         qry.selectPatientQuery("1", queries2,
                 new QueryCallback() {
+                    public void onCallback(HashMap<String, HashMap<String, Map<String, Object>>> data, String exception) {
+                        if (data == null)
+                            Log.e(TAG, exception);
+                        else
+                            Log.d(TAG, data.toString());
+                    }
+                });
+
+        qry.getPatientQuery("1", new QueryCallback() {
+            @Override
+            public void onCallback(HashMap<String, HashMap<String, Map<String, Object>>> data, String exception) {
+                if (data == null)
+                    Log.e(TAG, exception);
+                else
+                    Log.d(TAG, data.toString());
+            }
+        });
+
+        qry.getArchetypeQuery("openEHR-EHR-EVALUATION.SOAP_Assessment_RCP.v3.adl", new QueryCallback() {
+            @Override
+            public void onCallback(HashMap<String, HashMap<String, Map<String, Object>>> data, String exception) {
+                if (data == null)
+                    Log.e(TAG, exception);
+                else
+                    Log.d(TAG, data.toString());
+            }
+        });
+
+        qry.getPatientTimestampQuery("1", "1555404623", "1555404627", new QueryCallback() {
+            @Override
+            public void onCallback(HashMap<String, HashMap<String, Map<String, Object>>> data, String exception) {
+                if (data == null)
+                    Log.e(TAG, exception);
+                else
+                    Log.d(TAG, data.toString());
+            }
+        });
+
+        qry.getArchetypeTimestampQuery("openEHR-EHR-EVALUATION.SOAP_Assessment_RCP.v3.adl",
+                "1555404771", "1555404788", new QueryCallback() {
+                    @Override
+                    public void onCallback(HashMap<String, HashMap<String, Map<String, Object>>> data, String exception) {
+                        if (data == null)
+                            Log.e(TAG, exception);
+                        else
+                            Log.d(TAG, data.toString());
+                    }
+                });
+
+        qry.getPatientArchetypeTimestampQuery("1", "openEHR-EHR-EVALUATION.SOAP_Assessment_RCP.v3.adl",
+                "1555404622", "1555404623", new QueryCallback() {
+                    @Override
                     public void onCallback(HashMap<String, HashMap<String, Map<String, Object>>> data, String exception) {
                         if (data == null)
                             Log.e(TAG, exception);
--- medeformlibrary/src/main/java/com/hackslash/medeformlibrary/Queries.java
+++ medeformlibrary/src/main/java/com/hackslash/medeformlibrary/Queries.java
@@ -3,12 +3,15 @@ package com.hackslash.medeformlibrary;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -116,12 +119,80 @@ public class Queries {
         });
     }
 
-    public void getPatientQuery(String patientID) {
-
+    public void getPatientQuery(String patientID, final QueryCallback queryCallback) {
+        mFirestore.collection("EHR1").document("EHR1").collection(patientID)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    queryCallback.onCallback(extractData1(task), null);
+                } else {
+                    Log.e(TAG, task.getException().toString());
+                    queryCallback.onCallback(null, task.getException().toString());
+                }
+            }
+        });
     }
 
-    public void getArchetypeQuery(String archetypeName) {
+    public void getArchetypeQuery(String archetypeName, final QueryCallback queryCallback) {
+        mFirestore.collection("EHR2").document("EHR2").collection(archetypeName)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    queryCallback.onCallback(extractData1(task), null);
+                } else {
+                    Log.e(TAG, task.getException().toString());
+                    queryCallback.onCallback(null, task.getException().toString());
+                }
+            }
+        });
+    }
 
+    public void getPatientTimestampQuery(String patientID, final String startTimestamp, final String endTimestamp, final QueryCallback queryCallback) {
+        mFirestore.collection("EHR1").document("EHR1").collection(patientID)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    queryCallback.onCallback(extractData2(task, startTimestamp, endTimestamp), null);
+                } else {
+                    Log.e(TAG, task.getException().toString());
+                    queryCallback.onCallback(null, task.getException().toString());
+                }
+            }
+        });
+    }
+
+    public void getArchetypeTimestampQuery(String archetypeName, final String startTimestamp, final String endTimestamp, final QueryCallback queryCallback) {
+        mFirestore.collection("EHR2").document("EHR2").collection(archetypeName)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    queryCallback.onCallback(extractData2(task, startTimestamp, endTimestamp), null);
+                } else {
+                    Log.e(TAG, task.getException().toString());
+                    queryCallback.onCallback(null, task.getException().toString());
+                }
+            }
+        });
+    }
+
+    public void getPatientArchetypeTimestampQuery(String patientID, final String archetypeName, final String startTimestamp,
+                                                  final String endTimestamp, final QueryCallback queryCallback) {
+        mFirestore.collection("EHR1").document("EHR1").collection(patientID)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    queryCallback.onCallback(extractData3(task, archetypeName, startTimestamp, endTimestamp), null);
+                } else {
+                    Log.e(TAG, task.getException().toString());
+                    queryCallback.onCallback(null, task.getException().toString());
+                }
+            }
+        });
     }
 
     private HashMap<String, HashMap<String, Map<String, Object>>> extractData(List<DocumentSnapshot> docs) {
@@ -138,6 +209,67 @@ public class Queries {
                 data.put(pidOrArche, new HashMap() {{
                     put(timestamp, map);
                 }});
+            }
+        }
+        return data;
+    }
+
+    private HashMap<String, HashMap<String, Map<String, Object>>> extractData1(Task<QuerySnapshot> task) {
+        final HashMap<String, HashMap<String, Map<String, Object>>> data = new HashMap<>();
+        for (QueryDocumentSnapshot doc : task.getResult()) {
+            final Map<String, Object> map = doc.getData();
+            String[] key = doc.getId().split("@");
+            String pidOrArche = key[0];
+            final String timestamp = key[1];
+
+            if (data.containsKey(pidOrArche)) {
+                data.get(pidOrArche).put(timestamp, map);
+            } else {
+                data.put(pidOrArche, new HashMap() {{
+                    put(timestamp, map);
+                }});
+            }
+        }
+        return data;
+    }
+
+    private HashMap<String, HashMap<String, Map<String, Object>>> extractData2(Task<QuerySnapshot> task,
+                                                                               String startTimestamp, String endTimestamp) {
+        final HashMap<String, HashMap<String, Map<String, Object>>> data = new HashMap<>();
+        for (QueryDocumentSnapshot doc : task.getResult()) {
+            final Map<String, Object> map = doc.getData();
+            String[] key = doc.getId().split("@");
+            String pidOrArche = key[0];
+            final String timestamp = key[1];
+            if (timestamp.compareTo(startTimestamp) >= 0 && timestamp.compareTo(endTimestamp) <= 0) {
+                if (data.containsKey(pidOrArche)) {
+                    data.get(pidOrArche).put(timestamp, map);
+                } else {
+                    data.put(pidOrArche, new HashMap() {{
+                        put(timestamp, map);
+                    }});
+                }
+            }
+        }
+        return data;
+    }
+
+    private HashMap<String, HashMap<String, Map<String, Object>>> extractData3(Task<QuerySnapshot> task, String archetypeName,
+                                                                               String startTimestamp, String endTimestamp) {
+        final HashMap<String, HashMap<String, Map<String, Object>>> data = new HashMap<>();
+        for (QueryDocumentSnapshot doc : task.getResult()) {
+            final Map<String, Object> map = doc.getData();
+            String[] key = doc.getId().split("@");
+            String pidOrArche = key[0];
+            final String timestamp = key[1];
+            if (pidOrArche.equalsIgnoreCase(archetypeName) && timestamp.compareTo(startTimestamp) >= 0 && timestamp.compareTo(endTimestamp) <= 0) {
+                if (data.containsKey(pidOrArche)) {
+                    data.get(pidOrArche).put(timestamp, map);
+                } else {
+                    data.put(pidOrArche, new HashMap() {{
+                        put(timestamp, map);
+                    }});
+                }
             }
         }
         return data;
